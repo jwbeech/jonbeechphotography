684aaf30e971dd7a97aef6a35622b0dd0007d616
Jonathan Beech <jon@leftandright.co.za>
2015-08-19 22:06:42 +0200

    Minor work on the image viewer

---
+++ wordpress/wp-content/themes/jonbeechphotography/app/sections/viewer/viewer-directive.js
@@ -0,0 +1,12 @@
+angular.module("jonphoto").directive("viewer", [function(){
+	return {
+		templateUrl: "/wp-content/themes/jonbeechphotography/app/sections/viewer/viewer.html",
+		restrict: "E",
+		scope: {
+
+		},
+		link: function ($scope, $element, $attrs) {
+
+		}
+	}
+}]);
